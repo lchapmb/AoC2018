6b022742bc47ce45ff40d7efd153700600358a19
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-01-17 15:41:59 +0000

    improved separation of code

--- AoC2018/Program.cs
+++ AoC2018/Program.cs
@@ -9,29 +9,49 @@ namespace AoC2018
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Hello AoC!");
 
             // string testFreqInput = "+1, -2, +3, +1";
 
             using (StreamReader reader = new StreamReader("C:/Users/lewis.chapman-barker/source/repos/AoC2018/inputs/day 1.txt"))
             {
-                // declare starting frequency
-                int frequency = 0;
+                //int part1Result = Part1(reader);
+                //Console.WriteLine($"Part 1 result is {part1Result}");
 
-                // declare line variable
-                string line;
-
-                // loop through given date
-                while ((line = reader.ReadLine()) != null){
-
-                    // parse and add each line to frequency
-                    Console.WriteLine(line);
-                    frequency += Int32.Parse(line);
-                }
-
-                // write frequency to console
-                Console.WriteLine(frequency);
+                int part2Result = Part2(reader);
+                Console.WriteLine($"Part 2 result is {part2Result}");
             }
+        }
+
+        public static int Part1(StreamReader reader)
+        {
+            // declare starting frequency
+            int frequency = 0;
+
+            // declare line variable
+            string line;
+
+            // loop through given date
+            while ((line = reader.ReadLine()) != null)
+            {
+
+                // parse and add each line to frequency
+                // Console.WriteLine(line);
+                frequency += Int32.Parse(line);
+            }
+
+            // write frequency to console
+            // Console.WriteLine(frequency);
+
+            // return frequency
+            return frequency;
+        }
+
+        public static int Part2(StreamReader reader)
+        {
+            return 0;
         }
     }
 }
