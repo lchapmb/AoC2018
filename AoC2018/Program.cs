0dcb9d4207c4a826cf5b1787b4d704d4e910c181
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-01-17 15:35:19 +0000

    Day 1 first task

--- AoC2018/Program.cs
+++ AoC2018/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace AoC2018
 {
@@ -6,7 +9,29 @@ namespace AoC2018
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello AoC!");
+
+            // string testFreqInput = "+1, -2, +3, +1";
+
+            using (StreamReader reader = new StreamReader("C:/Users/lewis.chapman-barker/source/repos/AoC2018/inputs/day 1.txt"))
+            {
+                // declare starting frequency
+                int frequency = 0;
+
+                // declare line variable
+                string line;
+
+                // loop through given date
+                while ((line = reader.ReadLine()) != null){
+
+                    // parse and add each line to frequency
+                    Console.WriteLine(line);
+                    frequency += Int32.Parse(line);
+                }
+
+                // write frequency to console
+                Console.WriteLine(frequency);
+            }
         }
     }
 }
