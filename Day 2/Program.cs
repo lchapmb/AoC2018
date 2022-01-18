dc263b47bb7e79e5093b26e8c97f6989def1d9f3
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-01-18 09:02:40 +0000

    Saved inputs for day 2, created new project for day 2

---
+++ Day 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+                        int part1Result = Part1();
+
+            Console.Write($"Part 1 result is {part1Result}");
+        }
+
+        public static int Part1()
+        {
+            // declare int
+            int checksum = 0;
+
+            return checksum;
+        }
+    }
+}
